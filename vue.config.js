fe642523c7f93a9c0d2733cc7b7fd44cd1cebfe5
unknown <1812117655@qq.com>
2021-08-25 09:50:15 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configureWebpack:{
+    // 配置路径别名
+    alias:{
+      'assets':'@/assets',
+      'common':'@/common',
+      'components':'@/components',
+      'network':'@/network',
+      'views':'@/views',
+    }
+  }
+}
